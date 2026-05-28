ccfa7637cb478aae548bdddd85497fe3aa1bb1e9
baylealee <btokaahi@gmail.com>
2026-05-28 10:07:16 +0800

    Add frontend config for subscription endpoint

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+window.AI_TRENDS_CONFIG = {
+  // 之後把 Google Apps Script Web App URL 貼在這裡。
+  // 例：https://script.google.com/macros/s/xxxxx/exec
+  subscribeEndpoint: "",
+  defaultSubscriber: "Btokaa@msn.com"
+};
