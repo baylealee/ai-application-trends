3d61e0cc1317b08f368200f0d332323a046cdbb5
baylealee <btokaahi@gmail.com>
2026-05-28 11:07:03 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.AI_TRENDS_CONFIG = {
   // 之後把 Google Apps Script Web App URL 貼在這裡。
   // 例：https://script.google.com/macros/s/xxxxx/exec
-  subscribeEndpoint: "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRszRU5Gxbq4H5SzlHsgyN65cAjbiMJvka28o0x7_7RVo7p6zq49XXkz82mJNWUnq42W9_L1jv8-7GzxrnQoKpRnZw87aMB8DaivFAvdRmiArle6FbF-Zwb46kYK1k3O_V-omowysbXOxlIn_XEEtLQT5hDFB3s4h6s78QULQEm2F38EP1CwXSjuHok3cGoklVNzibsiNdv412Xwhz8N2xAQKDLk4vEOfKDSrDmVO-5IsOL7XavC5rt1GD6oQE3yQ35b5zrld58WnGxAkAzRsaVGPBbag&lib=MXXEI_6ehemMdiF6rgKDlAqtTICRyy3RE",
+  subscribeEndpoint: "https://script.google.com/macros/s/AKfycbyxu_zPqlA_Ku_cUIV_xg7ufjgwQWnAYZsK-POGX1yUTZ2ihqavlHKQs19VAWZdoUx6vQ/exec",
   defaultSubscriber: "Btokaa@msn.com"
 };
